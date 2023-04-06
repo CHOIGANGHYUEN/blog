d95f6b6c6708800bf42b5a51a316658e96fa4690
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-04-06 09:18:06 +0900

    awda

--- study/java/ex_0329/App.java
+++ study/java/ex_0329/App.java
@@ -1,7 +1,13 @@
 package study.java.ex_0329;
 
-public class App {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class App {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        try (ServerSocket serverSocket = new ServerSocket(8888)) {
+            System.out.println("클라이언트가 연결되었습니다.");
+        }
     }
 }
